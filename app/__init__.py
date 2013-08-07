db356dbfe7b5bab7d0a4d50fc26d2bcf570fd31e
Chris <chris.zhu12@gmail.com>
2013-08-06 19:24:47 -0700

    working version

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,7 @@
 from flask import Flask, session
-from db import db_con
 
 
 app = Flask(__name__)
 
 from app import views
 
-conn = db_con.Database()
-
-def current_user():
-  return conn.read("SELECT FROM Users WHERE user_id = %s" % (session['userid']))
