7605f3d1a7627144f8a43312d4edf41d792f831f
Chris <chris.zhu12@gmail.com>
2013-08-10 16:10:07 -0700

    added maanger buttons

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -3,7 +3,11 @@
 angular.module("ams", []).
   config(['$routeProvider', function($routeProvider){
     $routeProvider.when("/", {templateUrl:"static/partials/index.html", controller: IndexController}).
-    when("/admin", {templateUrl:"static/partials/view_admin.html", controller: AdminController}).
+    when("/manager", {templateUrl:"static/partials/manager_partials/view_manager.html", controller: ManagerController}).
+    when("/manager/add_items", {templateUrl:"static/partials/manager_partials/view_manager_add_items.html", controller: ManagerAddItemsController}).
+    when("/manager/sales_report", {templateUrl:"static/partials/manager_partials/view_manager_sales_report.html", controller: ManagerSalesReportController}).
+    when("/manager/top_items", {templateUrl:"static/partials/manager_partials/view_manager_top_items.html", controller: ManagerTopItemsController}).
+    when("/manager/process_delivery", {templateUrl:"static/partials/manager_partials/view_manager_process_delivery.html", controller: ManagerProcessDeliveryController}).
     when("/songs/:songUpc", {templateUrl:"static/partials/view_song.html", controller: SongController}).
     when("/clerk", {templateUrl:"static/partials/view_clerk.html", controller: ClerkController}).
     when("/clerk/register", {templateUrl:"static/partials/view_clerk_register.html", controller: ClerkRegisterController}).
